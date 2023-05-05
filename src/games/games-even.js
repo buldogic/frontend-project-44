7cab1302ec141de06ed2bc06fb827d0e462795a0
Afonin Andrey <andreyafonin96@gmail.com>
2023-05-05 16:34:25 +0300

    correction brain-even

--- src/games/games-even.js
+++ src/games/games-even.js
@@ -1,15 +1,14 @@
 import basisGame from "../index.js";
 import { numberRandom } from "../utils.js";
 
-const evenGames = () => {
-  const textStartGame = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-  basisGame(textStartGame, playEvenGame);
-};
-
 const playEvenGame = () => {
   const number = numberRandom(1, 99);
   const rightAnswer = number % 2 === 0 ? "yes" : "no";
   return [`${number}`, rightAnswer];
 };
 
+const evenGames = () => {
+  const textStartGame = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+  basisGame(textStartGame, playEvenGame);
+};
 export default evenGames;
